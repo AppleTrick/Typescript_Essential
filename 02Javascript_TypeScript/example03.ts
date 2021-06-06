99369bb87d056ca74e4fea31808ab374f41d3864
AppleTrick <arrans9591@gmail.com>
2021-06-06 23:46:11 +0900

    타입스크립트vs 자바스크립트

---
+++ 02Javascript_TypeScript/example03.ts
@@ -0,0 +1,11 @@
+let age = 10; // 10을 나이로 유추..
+let x = 10; // 10의 데이터가 불분명
+
+let weight:number = 80;
+
+type Centimeter = number;
+let height: Centimeter = 176;
+// 코드상에서 유형으로 표현 가능
+
+type RainbowColor = 'red' | 'orange' | 'yellow' | 'blue';
+let color : RainbowColor = 'orange';
