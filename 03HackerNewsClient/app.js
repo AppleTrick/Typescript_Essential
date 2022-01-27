89bb25d3141556df781c9c5b03edcad7bea36e9f
ChangHee Park <arrans9591@gmail.com>
2022-01-27 18:13:57 +0900

    해커스 뉴스 만들어보기

---
+++ 03HackerNewsClient/app.js
@@ -0,0 +1,19 @@
+const ajax = new XMLHttpRequest();
+
+// url 페이징에서 바뀔 가능성이 있음 => 바뀔 가능성이 있으면 
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
+
+ajax.open('GET',NEWS_URL,false);  // false => 동기로 처리
+ajax.send();
+
+const newsFeed = JSON.parse(ajax.response); // json으로 받아온것을 객체로 변환;
+
+const ul = document.createElement('ul');
+
+for (let i = 0; i < 10; i++) {
+    const li = document.createElement('li');
+    li.innerHTML = newsFeed[i].title;
+    ul.appendChild(li);
+}
+
+document.getElementById('root').appendChild(ul);
