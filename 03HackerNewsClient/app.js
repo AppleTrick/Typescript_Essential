1cc8cc546df90c979e7f84de8c83eabe905ecb2c
ChangHee Park <arrans9591@gmail.com>
2022-02-13 19:19:52 +0900

    mod - 코드 분리 뉴스목록 구성하는 함수로 묶기 클릭시 뉴스 내용 디테일하게 구현하는 함수로 묶기

--- 03HackerNewsClient/app.js
+++ 03HackerNewsClient/app.js
@@ -2,15 +2,13 @@ const container = document.getElementById('root');
 
 const ajax = new XMLHttpRequest();
 
-const content = document.createElement('div');
-
-// url 페이징에서 바뀔 가능성이 있음 => 바뀔 가능성이 있으면 
 // 뉴스들에 대한 정보 가지고 오는 url
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
 
 // content 정보 가지고 오는 url
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
+//ajax 데이터 통신함수
 function getData(url){
     ajax.open('GET',url,false);  // false => 동기로 처리
     ajax.send();
@@ -18,17 +16,33 @@ function getData(url){
     return JSON.parse(ajax.response); // json으로 받아온것을 객체로 변환;
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement('ul');
+// 뉴스피드 목록 구성하하는 함수
+function newsFeed(){
 
+    const newsFeed = getData(NEWS_URL);
 
-// 주소가 변경될때 작동하는 이벤트
-window.addEventListener('hashchange',function(){
-    
+    const newsList = [];
+
+    newsList.push('<ul>');
+    for (let i = 0; i < 10; i++) {
+        newsList.push(`
+        <li>
+            <a href = "#${newsFeed[i].id}"> 
+                ${newsFeed[i].title} (${newsFeed[i].comments_count}) 
+            </a>
+        </li>
+        `);
+    }
+    newsList.push('</ul>');
+
+    container.innerHTML = newsList.join('');
+
+}
+function newsDetail(){
+
     // 주소에 관련된 내용 가지고 오는법
     const id = location.hash.substring(1); // id 값을 가지고 옴
     const newsContent = getData(CONTENT_URL.replace('@id',id))
-    const title = document.createElement('h1');
 
     container.innerHTML = `
         <h1>${newsContent.title}</h1>
@@ -36,21 +50,10 @@ window.addEventListener('hashchange',function(){
             <a href="#">목록으로</a>
         </div>
     `;
-});
-
-const newsList = [];
-
-newsList.push('<ul>');
-for (let i = 0; i < 10; i++) {
-    newsList.push(`
-    <li>
-        <a href = "#${newsFeed[i].id}"> 
-            ${newsFeed[i].title} (${newsFeed[i].comments_count}) 
-        </a>
-    </li>
-    `);
 }
-newsList.push('</ul>');
 
-container.innerHTML = newsList.join('');
+// 주소가 변경될때 작동하는 이벤트
+window.addEventListener('hashchange', newsDetail);
+
+
 
