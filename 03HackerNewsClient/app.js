361e8da0d78865d4bed2ce4e517c23f63497bfec
ChangHee Park <arrans9591@gmail.com>
2022-02-21 15:10:09 +0900

    :smile: add - font-awesome cdn 추가 - template 내용 변경

--- 03HackerNewsClient/app.js
+++ 03HackerNewsClient/app.js
@@ -28,16 +28,28 @@ function newsFeed(){
     // 마킹해주기
     // m => margin x => 가로값 p=> padding
     let template = `
-        <div class="container mx-auto p-4">
-            <h1>Hacker News</h1>
-            <ul>
-                {{__news_feed__}}
-            </ul>
-            <div>
-                <a href="#/page/{{__preview_page__}}">이전으로</a> 
-                <a href="#/page/{{__next_page__}}">다음으로</a> 
+    <div class="bg-gray-600 min-h-screen">
+        <div class="bg-white text-xl">
+            <div class="mx-auto px-4">
+                <div class="flex justify-between items-center py-6">
+                    <div class="flex justify-start">
+                        <h1 class="font-extrabold">Hacker News</h1>
+                    </div>
+                    <div class="items-center justify-end">
+                        <a href="#/page/{{__prev_page__}}" class="text-gray-500">
+                        Previous
+                        </a>
+                        <a href="#/page/{{__next_page__}}" class="text-gray-500 ml-4">
+                        Next
+                        </a>
+                    </div>
+                </div> 
             </div>
         </div>
+        <div class="p-4 text-2xl text-gray-700">
+            {{__news_feed__}}        
+        </div>
+    </div>
     `
 
 
